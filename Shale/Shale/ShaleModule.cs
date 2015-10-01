f38563f79f19af1be1e275c962a0acbdfbf45c4e
vazgmik <vazgvazg2@gmail.com>
2015-10-01 17:36:34 +0300

    [+]Create Project

---
+++ Shale/Shale/ShaleModule.cs
@@ -0,0 +1,79 @@
+using System;
+using Slb.Ocean.Core;
+using Slb.Ocean.Petrel;
+using Slb.Ocean.Petrel.UI;
+using Slb.Ocean.Petrel.Workflow;
+
+namespace Shale
+{
+    /// <summary>
+    /// This class will control the lifecycle of the Module.
+    /// The order of the methods are the same as the calling order.
+    /// </summary>
+    public class ShaleModule : IModule
+    {
+        public ShaleModule()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        #region IModule Members
+
+        /// <summary>
+        /// This method runs once in the Module life; when it loaded into the petrel.
+        /// This method called first.
+        /// </summary>
+        public void Initialize()
+        {
+            // TODO:  Add ShaleModule.Initialize implementation
+        }
+
+        /// <summary>
+        /// This method runs once in the Module life. 
+        /// In this method, you can do registrations of the not UI related components.
+        /// (eg: datasource, plugin)
+        /// </summary>
+        public void Integrate()
+        {
+            
+            // TODO:  Add ShaleModule.Integrate implementation
+        }
+
+        /// <summary>
+        /// This method runs once in the Module life. 
+        /// In this method, you can do registrations of the UI related components.
+        /// (eg: settingspages, treeextensions)
+        /// </summary>
+        public void IntegratePresentation()
+        {
+
+            // TODO:  Add ShaleModule.IntegratePresentation implementation
+        }
+
+        /// <summary>
+        /// This method called once in the life of the module; 
+        /// right before the module is unloaded. 
+        /// It is usually when the application is closing.
+        /// </summary>
+        public void Disintegrate()
+        {
+            // TODO:  Add ShaleModule.Disintegrate implementation
+        }
+
+        #endregion
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            // TODO:  Add ShaleModule.Dispose implementation
+        }
+
+        #endregion
+
+    }
+
+
+}
